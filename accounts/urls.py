42493d4fdb18eb9a2c22d8149893aa38e6b5c974
Krishn2607 <krishnkarelia@gmail.com>
2026-02-18 16:04:52 +0530

    Resolved merge conflict in accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import UserLoginView
-from django.contrib.auth.views import LogoutView
-=======
 from .views import UserRegisterView, UserLoginView, UserLogoutView
->>>>>>> 57f8c8a6c814a38c0e4536d2c406f487cddb3ece
 
 app_name = "accounts"
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(next_page="/accounts/login/"), name="logout"),
-=======
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
->>>>>>> 57f8c8a6c814a38c0e4536d2c406f487cddb3ece
 ]
